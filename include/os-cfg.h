3176fe9b4841bf5188d90ae9de0cff1d3bee09f7
onhangocnhu <nhu.onhangoc@hcmut.edu.vn>
2025-04-24 18:42:08 +0700

    Update os-cfg.h

--- include/os-cfg.h
+++ include/os-cfg.h
@@ -1,14 +1,15 @@
 #ifndef OSCFG_H
 #define OSCFG_H
 
-#define MLQ_SCHED 1 // chung, nào cũng phải có
+#define MLQ_SCHED 1
 #define MAX_PRIO 140
 
-#define MM_PAGING      // chung, nào cũng phải có
+#define MM_PAGING
 #define MM_FIXED_MEMSZ // ./os sched, ./os sched_0, ./os sched_1, ./os os_1_singleCPU_mlq
-#define VMDBG 1        // còn lại
-#define MMDBG 1        // còn lại
-#define IODUMP 1       // print DUMP
-#define PAGETBL_DUMP 1 // Print page table
-#define TIME_SLOT 1    // Print time slot
+#define VMDBG 1
+#define MMDBG 1
+#define IODUMP 1       // Print I/O Dump
+#define PAGETBL_DUMP 1 // Print Page Table Dump
+#define TIME_SLOT 1    // Print Time Slot
+
 #endif
