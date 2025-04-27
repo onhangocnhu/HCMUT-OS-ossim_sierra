000b046879cd505a0eca29cdcf57d8ff78e3ff5b
onhangocnhu <nhu.onhangoc@hcmut.edu.vn>
2025-04-27 16:43:34 +0700

    config default

--- include/os-cfg.h
+++ include/os-cfg.h
@@ -7,8 +7,8 @@
 #define MM_PAGING
 #define MM_FIXED_MEMSZ
 #define MMDBG          // Print Memory Management Debug
-#define VMDBG          // Print Virtual Memory Debug
-#define IODUMP 1       // Print I/O Dump
+// #define VMDBG          // Print Virtual Memory Debug
+// #define IODUMP 1       // Print I/O Dump
 #define PAGETBL_DUMP 1 // Print Page Table Dump
 #define TIME_SLOT 1    // Print Time Slot
 
